6be56e247075935c49bc21d2665f01dd33486dcb
HudsonPeres <hudsonaap@gmail.com>
2026-03-23 11:28:57 +0000

    separei places

---
+++ src/data/places.js
@@ -0,0 +1,17 @@
+const places = [
+  [{ lat: 38.706871, lng: -9.136196 }, { city: 'Lisboa' }],
+  [{ lat: 48.858093, lng: 2.294694 }, { city: 'Paris' }],
+  [{ lat: 51.505946, lng: -0.120314 }, { city: 'Londres' }],
+  [{ lat: 25.198546, lng: 55.273276 }, { city: 'Dubai' }],
+  [{ lat: -33.851192, lng: 151.217172 }, { city: 'Sidney' }],
+  [{ lat: 29.976768, lng: 31.135538 }, { city: 'Cairo' }],
+  [{ lat: 40.757876, lng: -73.985592 }, { city: 'Nova York' }],
+  [{ lat: 40.280215, lng: -7.507109 }, { city: 'Covilhã' }],
+  [{ lat: -5.870383, lng: -35.181368 }, { city: 'Natal' }],
+  [{ lat: 41.148416, lng: -8.647584 }, { city: 'Porto' }],
+  [{ lat: -5.916738, lng: -35.260632 }, { city: 'Parnamirim'}],
+  [{ lat: -5.752831, lng: -35.202848 }, { city: 'Natal'}],
+  [{ lat: -5.142992, lng: -36.610094 }, { city: 'Macau'}],
+];
+
+export default places;
